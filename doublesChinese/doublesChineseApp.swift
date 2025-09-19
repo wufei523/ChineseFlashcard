e8ae6d33fd7a43af90b36bed2561c34e974167f9
Wu, Fei <feiwu@copacld.com>
2025-09-19 16:37:34 -0700

    first commit

---
+++ doublesChinese/doublesChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  doublesChineseApp.swift
+//  doublesChinese
+//
+//  Created by Wu, Fei on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct doublesChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ doublesChineseTests/doublesChineseTests.swift
@@ -0,0 +1,17 @@
+//
+//  doublesChineseTests.swift
+//  doublesChineseTests
+//
+//  Created by Wu, Fei on 9/12/25.
+//
+
+import Testing
+@testable import doublesChinese
+
+struct doublesChineseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
